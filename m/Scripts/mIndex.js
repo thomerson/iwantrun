4889171e903d7b818df1d3741c9afb7204085832
thomerson <tanghongsheng123@126.com>
2019-01-06 21:57:01 +0800

    update

--- m/Scripts/mIndex.js
+++ m/Scripts/mIndex.js
@@ -139,11 +139,24 @@ var appIndex = new Vue(
                     errMsg: null,
                     autoLogin: false,
                     password: null
+                },
+                listcase: {
+                    searchlist: {},
+                    param: {
+                        pageIndex: 1,
+                        pageSize: 10,
+                        duration: [],
+                        activitytype: [],
+                        personNum: []
+                    },
+                    cases: [],
+                    showbtnmore: true
                 }
             },
             show: {
                 sildeMenu: false,
-                search: false
+                search: false,
+                filter: false
             },
             SMS: {
                 timer: null,
@@ -188,6 +201,18 @@ var appIndex = new Vue(
                 vm.currentpage = page;
                 vm.show.sildeMenu = false;
                 window.scrollTo(0, 0);
+
+                var init = {
+                    listcase: function () {
+                        //if (!vm.model.listcase.searchlist) {
+                        vm.getcaseSearchList();
+                        vm.queryCase();
+                        //}
+                    }
+                };
+                if (typeof init[page] === 'function') {
+                    init[page]();
+                }
             },
             setCurrencity: function () {
                 var vm = this, city = jQuery.cookie('currentcity');
@@ -440,13 +465,84 @@ var appIndex = new Vue(
                         }
                     })
             },
-            submitOrder: function () { }, //提交需求 TODO
-            showSearch: function () {
+            submitOrder: function () { }, //提交需求 TODO,
+            getcaseSearchList: function () {
+                var vm = this, url = "../" + requestUrl.caseSearchList, param = { name: 'common' };
+                axios.post(url, param).then(
+                    function (response) {
+                        console.log(response.data);
+                        vm.model.listcase.searchlist = response.data;
+                    })
+            },
+            resetCase: function () {
                 var vm = this;
-                //console.log('test');
-                vm.show.search = true;
-                //vm.setCurrentPage('search');
-            } //显示搜索栏
+                vm.model.listcase.param = {
+                    pageIndex: 0,
+                    pageSize: 10,
+                    duration: [],
+                    activitytype: [],
+                    personNum: []
+                };
+                $('.icon-checkbox-blank').removeClass('icon-checkbox-blank');
+            },
+            queryCase: function (pageIndex) {
+                var vm = this, url = "../" + requestUrl.queryCaseByCondition, param = vm.model.listcase.param;
+                //param.pageIndex = pageIndex - 1;
+                axios.post(url, param).then(
+                    function (response) {
+                        //console.log(response.data);
+                        vm.model.listcase.cases = response.data.content;
+                        vm.model.listcase.showbtnmore = response.data.content.length == vm.model.listcase.param.pageSize;
+                    })
+            },
+            getMoreCase: function () {
+                var vm = this;
+                vm.model.listcase.param.pageIndex += 1;
+                vm.queryCase();
+            },
+            activitytypechange: function ($event, id) {
+                //console.log($event.target);
+                //console.log(id);
+                var vm = this, $dom = $($event.target);
+                console.log($dom);
+                if ($dom.hasClass('icon-checkbox-blank')) {
+                    $dom.removeClass('icon-checkbox-blank');
+                    var index = vm.model.listcase.param.activitytype.indexOf(id);
+                    vm.model.listcase.param.activitytype.splice(index, 1);
+                } else {
+                    $dom.addClass('icon-checkbox-blank');
+                    vm.model.listcase.param.activitytype.push(id);
+                }
+                //console.log(vm.model.listcase.param.activitytype);
+                vm.queryCase("1");
+
+            },
+            durationchange: function ($event, id) {
+                var vm = this, $dom = $($event.target);
+                console.log($dom);
+                if ($dom.hasClass('icon-checkbox-blank')) {
+                    $dom.removeClass('icon-checkbox-blank');
+                    var index = vm.model.listcase.param.duration.indexOf(id);
+                    vm.model.listcase.param.duration.splice(index, 1);
+                } else {
+                    $dom.addClass('icon-checkbox-blank');
+                    vm.model.listcase.param.duration.push(id);
+                }
+                vm.queryCase("1");
+            },
+            personNumchange: function ($event, id) {
+                var vm = this, $dom = $($event.target);
+                console.log($dom);
+                if ($dom.hasClass('icon-checkbox-blank')) {
+                    $dom.removeClass('icon-checkbox-blank');
+                    var index = vm.model.listcase.param.personNum.indexOf(id);
+                    vm.model.listcase.param.personNum.splice(index, 1);
+                } else {
+                    $dom.addClass('icon-checkbox-blank');
+                    vm.model.listcase.param.personNum.push(id);
+                }
+                vm.queryCase("1");
+            },
         },
         components: {
             companyfooter: { template: '#companyfooter' }
@@ -458,5 +554,7 @@ var appIndex = new Vue(
             vm.queryCaseByCondition(1);
             vm.queryProdutionByCondition(1);
             vm.queryLocationByCondition(1);
+
+            //vm.setCurrentPage('listcase');
         }
     });
